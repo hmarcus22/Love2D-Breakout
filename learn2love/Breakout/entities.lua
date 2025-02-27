ff2e3efb6768864435235403a504ae4b63b256c8
hmarcus22 <h.marcus22@gmail.com>
2025-02-27 17:00:10 +0100

    Added input entity

---
+++ learn2love/Breakout/entities.lua
@@ -0,0 +1,19 @@
+local boundry_bottom = require('entities/boundry-bottom')
+local boundry_vertical = require('entities/boundry-vertical')
+local boundry_top = require('entities/boundry-top')
+local paddle = require('entities/paddle')
+local ball = require('entities/ball')
+local world = require('world')
+local brick = require('entities/brick')
+
+return {
+    boundry_bottom(400, 606),
+    boundry_vertical(-6, 300),
+    boundry_vertical(806, 300),
+    boundry_top(400, -6),
+    ball(200, 200),
+    paddle(300, 500),
+    brick(100, 100),
+    brick(200, 100),
+    brick(300, 100)
+}
--- learn2love/Breakout/main.lua
+++ learn2love/Breakout/main.lua
@@ -1,35 +1,6 @@
-local boundry_bottom = require('entities/boundry-bottom')
-local boundry_left = require('entities/boundry-left')
-local boundry_right = require('entities/boundry-right')
-local boundry_left = require('entities/boundry-left')
-local boundry_top = require('entities/boundry-top')
-local paddle = require('entities/paddle')
-local ball = require('entities/ball')
+local entities = require('entities')
 local world = require('world')
-local brick = require('entities/brick')
-
-local entities = {
-    boundry_bottom(400, 606),
-    boundry_left(-6, 300),
-    boundry_right(806, 300),
-    boundry_top(400, -6),
-    ball(200, 200),
-    paddle(300, 500),
-    brick(100, 100),
-    brick(200, 100),
-    brick(300, 100)
-}
-
-local paused = false
-
-local key_map = {
-    escape = function()
-        love.event.quit()
-    end,
-    space = function()
-        paused = not paused
-    end
-}
+local input = require('input')
 
 love.focus = function(f)
     if not f then
@@ -52,13 +23,19 @@ love.draw = function()
 end
 
 love.keypressed = function(key_pressed)
-    if key_map[key_pressed] then
-        key_map[key_pressed]()
-    end
+    input.press(pressed_key)
+end
+
+love.keyreleased = function(released_key)
+    input.release(released_key)
 end
 
 love.update = function(dt)
-    if not paused then
+    if not input.paused then
+        for _, entity in ipairs(entities) do
+            if entity.update then entity:update(dt) end
+        end
         world:update(dt)
     end
 end
+  
