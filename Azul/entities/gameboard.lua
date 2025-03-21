ca13b6a8371f306a4de73ea27d8636fa557a7346
hmarcus22 <h.marcus22@gmail.com>
2025-03-21 15:18:28 +0100

    working on gameboard

--- Azul/entities/gameboard.lua
+++ Azul/entities/gameboard.lua
@@ -4,9 +4,15 @@ local Gameboard = Entity:extend()
 
     function Gameboard:new()
         Gameboard.super.new(self, id)
-        self.tileBoard = {}
-        self.tileInput = {}
+        self.tileMatrix = { {1}, {2}, {3}, {4}, {5},
+                            {5}, {1}, {2}, {3}, {4},
+                            {4}, {5}, {1}, {2}, {3},
+                            {3}, {4}, {5}, {1}, {2},
+                            {2}, {3}, {4}, {5}, {1} }
+        self.tileInput = { {}, {}, {}, {}, {} }
         self.minusRow = {}
+        self.selectedTiles =  {}
+        self.selectedRow = 0
         self.width = love.graphics.getWidth() - 40
         self.height = (love.graphics.getHeight() - 40) / 2
         self.x = (self.width +40) / 2
@@ -20,9 +26,27 @@ local Gameboard = Entity:extend()
         
     end
     
+    function Gameboard:selecedtTiles()
+
+        
+    end
+    
     function Gameboard:addTiles(tileSet, row)
 
 
+    end
+
+    -- Untested
+    function Gameboard:insertTiles(tiles)
+        for index = 1 , #tiles do
+            if index <= #tiles then
+                self.tileInput[self.selectedRow] = tiles[index].id
+            else
+                for _, tile in pairs(tiles) do
+                    table.insert(self.minusRow, tile.id)
+                end
+            end
+        end
     end
 
     function Gameboard:resolveBoard()
@@ -48,3 +72,14 @@ local Gameboard = Entity:extend()
     
 
     return Gameboard
+
+    --Gamebord has input rows, first is 1 tile second is 2 .... last is 5 tiles.
+    --Has tilematrix, 5 x 5 tiles. each row has a spcesified place for 1 tile of each tiletype.
+    
+    --Should take ownership of tiles when selected from bucket.
+    
+    --Add tiles - Fill an input row with all selected tiles. If full put leftovers in minusrow
+
+    --Moves tiles from inputrow to tilematrix att end of round and counts score.
+    --subtracts potential minus score last.
+    
