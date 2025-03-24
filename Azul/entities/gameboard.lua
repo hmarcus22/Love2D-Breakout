da47b0c026bc7703ee21f92d2fbda153e40602b7
hmarcus22 <h.marcus22@gmail.com>
2025-03-24 16:52:19 +0100

    Added selected row to gameboard

--- Azul/entities/gameboard.lua
+++ Azul/entities/gameboard.lua
@@ -23,7 +23,73 @@ local Gameboard = Entity:extend()
         self.fixture:setUserData(self)
         self.contact = false
         self.offsetX = 80
+        self.selectedRow = 0
+        self.isRow = false
     end
+    
+    function Gameboard:update(dt)
+        --If choosen chase mouse
+       --Set target
+       local tileCount = 0
+       local step = 60
+       for _, tile in pairs(self.ownedEntities) do
+           if tile.choosen then
+               tileCount = tileCount +1
+               local offX = tileCount * step
+               tile.targetX = love.mouse.getX() - offX
+               tile.targetY = love.mouse.getY()
+           
+               --Chase target smoothly
+               if tile.body:getX() ~= tile.targetX or tile.body:getY() ~= tile.targetY then
+                   
+                   local dx = tile.targetX - tile.body:getX()
+                   local dy = tile.targetY - tile.body:getY()
+                   local newX = tile.body:getX() + dx * tile.speed * dt
+                   local newY = tile.body:getY() + dy * tile.speed * dt
+                   
+                   tile.body:setPosition(newX, newY)
+                   tile.x = newX
+                   tile.y = newY
+               end
+           end
+       end
+       self.selectedRow = 0
+       self.isRow = false
+       for ir, row in ipairs(self.tileInput) do
+           for is, square in ipairs(row) do
+               if square.fixture:testPoint(love.mouse.getPosition()) then
+                   self.selectedRow = ir
+                   self.isRow = true
+                   break
+               end
+           end
+       end
+   end
+
+   function Gameboard:draw()
+    if state.curentPlayer == self.player then
+        love.graphics.print('Player' .. self.player, self.x, self.y, 0, 1, 1, self.width * .5 - 10, self.height * .5 -10)
+        love.graphics.print('Selected row: ' .. self.selectedRow, self.x, self.y, 0, 1, 1, self.width * .5 - 10, self.height * .5 -25)
+        love.graphics.polygon('line', self.body:getWorldPoints(self.shape:getPoints()))
+        love.graphics.print(tostring(self.contact), self.x + 40, self.y - 40)
+        for _, row  in ipairs(self.tileMatrix) do
+            for _, square in pairs(row) do
+                love.graphics.polygon('line', square.body:getWorldPoints(square.shape:getPoints()))
+                -- print(square.x, square.y)
+            end
+        end
+        for _, row  in ipairs(self.tileInput) do
+            for _, square in pairs(row) do
+                love.graphics.polygon('line', square.body:getWorldPoints(square.shape:getPoints()))
+                -- print(square.x, square.y)
+            end
+        end
+        for _, square in pairs(self.minusRow) do
+            love.graphics.polygon('line', square.body:getWorldPoints(square.shape:getPoints()))
+            -- print(square.x, square.y)
+        end
+    end
+end
     
     function Gameboard:initBoard()
         self:initTileMatrix()
@@ -120,57 +186,9 @@ local Gameboard = Entity:extend()
         
     end
 
-    function Gameboard:update(dt)
-         --If choosen chase mouse
-        --Set target
-        local tileCount = 0
-        local step = 60
-        for _, tile in pairs(self.ownedEntities) do
-            if tile.choosen then
-                tileCount = tileCount +1
-                local offX = tileCount * step
-                tile.targetX = love.mouse.getX() - offX
-                tile.targetY = love.mouse.getY()
-            
-                --Chase target smoothly
-                if tile.body:getX() ~= tile.targetX or tile.body:getY() ~= tile.targetY then
-                    
-                    local dx = tile.targetX - tile.body:getX()
-                    local dy = tile.targetY - tile.body:getY()
-                    local newX = tile.body:getX() + dx * tile.speed * dt
-                    local newY = tile.body:getY() + dy * tile.speed * dt
-                    
-                    tile.body:setPosition(newX, newY)
-                    tile.x = newX
-                    tile.y = newY
-                end
-            end
-        end
-    end
+    
 
-    function Gameboard:draw()
-        if state.curentPlayer == self.player then
-            love.graphics.print('Player' .. self.player, self.x, self.y, 0, 1, 1, self.width * .5 - 10, self.height * .5 -10)
-            love.graphics.polygon('line', self.body:getWorldPoints(self.shape:getPoints()))
-            love.graphics.print(tostring(self.contact), self.x + 40, self.y - 40)
-            for _, row  in ipairs(self.tileMatrix) do
-                for _, square in pairs(row) do
-                    love.graphics.polygon('line', square.body:getWorldPoints(square.shape:getPoints()))
-                    -- print(square.x, square.y)
-                end
-            end
-            for _, row  in ipairs(self.tileInput) do
-                for _, square in pairs(row) do
-                    love.graphics.polygon('line', square.body:getWorldPoints(square.shape:getPoints()))
-                    -- print(square.x, square.y)
-                end
-            end
-            for _, square in pairs(self.minusRow) do
-                love.graphics.polygon('line', square.body:getWorldPoints(square.shape:getPoints()))
-                -- print(square.x, square.y)
-            end
-        end
-    end
+   
 
     function Gameboard:begin_contact()
         print('contact!')
